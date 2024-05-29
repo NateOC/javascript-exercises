82838e2e5e11486fc4658651e2bbc31b9069cf61
NateOC <153658693+NateOC@users.noreply.github.com>
2024-05-29 12:08:11 -0400

    finsihed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    string = "";
+    if(num < 0)
+            return "ERROR"
+    for(i = 0; i < num; i++)
+        {
+            string+=word;
+        }
+    return string;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
-
-};
-
+const palindromes = function (string) {
+// all valid things
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  
+    // t o lower case and compile into array of only valid characters
+    const cleanedString = string
+      .toLowerCase()
+      .split('')
+      .filter((character) => alphanumerical.includes(character))
+      .join('');
+  
+    // reverse to compare
+    const reversedString = cleanedString.split('').reverse().join('');
+  
+    // true or false if it matches or not
+    return cleanedString === reversedString;
+  };
+  
 // Do not edit below this line
 module.exports = palindromes;
